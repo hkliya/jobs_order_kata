d5c8b001a5d566526b549ddd6e61aa9206d678a2
Seaborn Lee <seabornlee.cn@gmail.com>
2015-02-02 10:09:27 +0800

    optimize failure message

--- spec/matcher.rb
+++ spec/matcher.rb
@@ -4,4 +4,8 @@ RSpec::Matchers.define :have_order do |first_job, second_job|
   match do |sequence|
     sequence.index(first_job) < sequence.index(second_job)
   end
+
+  failure_message_for_should do |actual|
+    "expected that #{actual} to have job '#{first_job}' before '#{second_job}'"
+  end
 end
