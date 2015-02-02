3dbba6cf3975040183e1fb46e4530fb5397e268d
Seaborn Lee <seabornlee.cn@gmail.com>
2015-02-02 11:22:01 +0800

    introduce instance

--- lib/jobs_order.rb
+++ lib/jobs_order.rb
@@ -1,37 +1,41 @@
 class JobsOrder
-  class << self
-    def process(jobs_list)
-      return [] if jobs_list.empty?
-      jobs = parse_jobs jobs_list
-      order jobs
-    end
+  def initialize(jobs_description)
+    @jobs = parse jobs_description
+  end
 
-    private
-    def order(jobs)
-      result = []
-      jobs.each { |job_name, dependency|
-        result << dependencies(jobs, dependency)
-        result << job_name
-      }
-      result.flatten
-    end
+  def order
+    result = []
+    @jobs.each { |job_name, dependency|
+      result << dependencies_of(dependency)
+      result << job_name
+    }
+    result.flatten
+  end
 
-    def parse_jobs(jobs_list)
-      jobs = {}
-      jobs_list.split(/\n/).each { |job_definition| 
-        splitted = job_definition.strip().split('=>')
-        job_name = splitted[0].strip()
-        dependency = splitted[1]
-        dependency.strip! unless dependency.nil?
-        jobs.store job_name, dependency
-      }
-      jobs
-    end
+  def self.process(jobs_description)
+    JobsOrder.new(jobs_description).order()
+  end
 
-    def dependencies(jobs, dependency)
-      return [] if dependency.nil?
-      result = dependencies jobs, jobs[dependency]
-      result << dependency
-    end
+  private
+  def parse(jobs_list)
+    jobs = {}
+    jobs_list.split(/\n/).each { |job_definition| 
+      splitted = job_definition.strip().split('=>')
+      job_name = splitted[0].strip()
+      dependency = splitted[1]
+      dependency.strip! unless dependency.nil?
+      jobs.store job_name, dependency
+    }
+    jobs
+  end
+
+  def dependencies_of(job)
+    return [] if job.nil?
+    result = dependencies_of dependency_of(job)
+    result << job
+  end
+
+  def dependency_of(job)
+    @jobs[job]
   end
 end
