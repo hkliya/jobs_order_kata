c77867d8b349d7f4af22afaeff002087fb3ddec5
Seaborn Lee <seabornlee.cn@gmail.com>
2015-02-02 11:03:27 +0800

    extract methods

--- lib/jobs_order.rb
+++ lib/jobs_order.rb
@@ -1,14 +1,37 @@
 class JobsOrder
-  def self.process(jobs_list)
-    return [] if jobs_list.empty?
-    result = []
-    jobs_list.split(/\n/).each { |job_definition| 
-      splitted = job_definition.strip().split('=>')
-      job_name = splitted[0].strip()
-      dependency = splitted[1]
-      result << dependency.strip() unless dependency.nil?
-      result << job_name
-    }
-    result
+  class << self
+    def process(jobs_list)
+      return [] if jobs_list.empty?
+      jobs = parse_jobs jobs_list
+      order jobs
+    end
+
+    private
+    def order(jobs)
+      result = []
+      jobs.each { |job_name, dependency|
+        result << dependencies(jobs, dependency)
+        result << job_name
+      }
+      result.flatten
+    end
+
+    def parse_jobs(jobs_list)
+      jobs = {}
+      jobs_list.split(/\n/).each { |job_definition| 
+        splitted = job_definition.strip().split('=>')
+        job_name = splitted[0].strip()
+        dependency = splitted[1]
+        dependency.strip! unless dependency.nil?
+        jobs.store job_name, dependency
+      }
+      jobs
+    end
+
+    def dependencies(jobs, dependency)
+      return [] if dependency.nil?
+      result = dependencies jobs, jobs[dependency]
+      result << dependency
+    end
   end
 end
